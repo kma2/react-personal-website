ec39a56dc3ea094367097d14b2e8f3a4427be719
Katy Ma <kma2@wellesley.edu>
2019-01-11 18:06:44 -0500

    Updates

---
+++ precache-manifest.a48128dce3ce11ad26b64b8de7e8d26f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "06eaaf159bcb64fb0ac1",
+    "url": "/react-test/static/css/main.84203a2f.chunk.css"
+  },
+  {
+    "revision": "06eaaf159bcb64fb0ac1",
+    "url": "/react-test/static/js/main.06eaaf15.chunk.js"
+  },
+  {
+    "revision": "d54be5b34eb34b2a9441",
+    "url": "/react-test/static/css/1.56b33f7d.chunk.css"
+  },
+  {
+    "revision": "d54be5b34eb34b2a9441",
+    "url": "/react-test/static/js/1.d54be5b3.chunk.js"
+  },
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-test/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-test/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-test/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-test/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-test/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "01c3ac7068b36e9a3fb3b1bba518f85d",
+    "url": "/react-test/index.html"
+  }
+];
